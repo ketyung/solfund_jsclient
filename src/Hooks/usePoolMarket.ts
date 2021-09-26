3c559d9351b890951b170c7615dcb2050f971a48
techchee <ketyung@techchee.com>
2021-09-26 09:48:04 +0800

    update

--- src/Hooks/usePoolMarket.ts
+++ src/Hooks/usePoolMarket.ts
@@ -24,6 +24,47 @@ export default function usePoolMarket(){
 
     async function createPoolMarketAccount( completionHandler : (result : boolean | Error) => void){
 
+        if (!publicKey){
+            completionHandler(new Error("No wallet connected"));
+            setLoading(false);
+            return; 
+        }
+
+        setLoading(true);
+
+        let size : number  = 322; // hard-coded first 
+
+        let marketPkey = await poolMarketIdPubKey();
+
+        let acc = await connection.getAccountInfo(marketPkey);
+        // create only when it's null
+        if ( acc == null ){
+
+            await createAccount(publicKey, publicKey, marketPkey, programId, POOL_MARKET_ID, size, 
+            (res : boolean | Error) =>  {
+
+                if (res instanceof Error){
+        
+                    completionHandler(res);
+                    setLoading(false);
+        
+                }
+                else {
+        
+                    completionHandler(true);
+                    setLoading(false);        
+                }
+        
+            });
+        }
+        else {
+
+            completionHandler(true);
+            setLoading(false);        
+    
+        }
+
+       
 
     }
 
