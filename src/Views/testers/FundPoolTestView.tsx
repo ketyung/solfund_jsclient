c2100e331989c270b91f006211eb1e2e8be1ca21
techchee <ketyung@techchee.com>
2021-09-27 15:57:49 +0800

    update

--- src/Views/testers/FundPoolTestView.tsx
+++ src/Views/testers/FundPoolTestView.tsx
@@ -1,2 +1,61 @@
 import React, {useState} from 'react';
 import 'antd/dist/antd.css';
+import useFundPool from '../../Hooks/useFundPool';
+import { Button, Spin, Modal } from 'antd';
+import { success,error } from '../../utils/Mesg';
+import { Wallet } from "../Wallet";
+
+export const FundPoolTesterView : React.FC = () => {
+
+
+    const [createFundPoolAccount, createFundPool, loading] = useFundPool();
+
+    const [modelPresented, setModalPresented] = useState(false);
+
+    const completion = (res : boolean | Error) =>  {
+
+        if (res instanceof Error){
+
+            error((res as Error).message, 5 );
+
+        }
+        else {
+
+            success("Success!", 5);
+
+        }
+    }
+
+    return <div>
+        <Wallet/>
+          <br/>
+          <br/>
+          <div style={{display: loading ? "block" : "none", margin : "10px"}}><Spin size="large"/></div>
+
+          <p><Button className="commonButton" danger onClick={async ()=> {
+              
+              createFundPoolAccount(completion);
+
+          }} >Create Fund Pool Account</Button></p>
+       
+         <p><Button className="commonButton" danger onClick={async ()=> {
+              
+              setModalPresented(true);
+
+          }} >Create Fund Pool</Button></p>
+       
+
+
+       <Modal title="Create Fund Pool"
+          style={{minWidth:"80%"}}
+          visible={modelPresented}
+          onCancel={()=>{setModalPresented(false);}}
+          okButtonProps={{ disabled: true }}
+          cancelButtonProps={{ disabled: false }}>
+       
+       
+       </Modal>
+
+    </div>;
+
+}
