692e04dfb56d76e19f635cc64f574ef46d732df3
techchee <ketyung@techchee.com>
2021-09-29 20:19:57 +0800

    update

--- src/Views/components/FundPoolView.tsx
+++ src/Views/components/FundPoolView.tsx
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import useFundPool from '../../Hooks/useFundPool';
+import {FundPool} from '../../state';
+import { error } from '../../utils/Mesg';
 
 interface FundPoolViewProps {
 
@@ -10,7 +12,28 @@ export const FundPoolView : React.FC <FundPoolViewProps> = ({address}) => {
 
     const [,,loading, read] = useFundPool();
 
+    const [fundPool, setFundPool] = useState<FundPool>();
 
+    useEffect(() => {
+        
+        read (address, 
+            
+            (res : FundPool | Error) =>  {
+
+                if (res instanceof Error){
+        
+                    error(res.message);
+                }
+                else {
+        
+                    setFundPool(res);
+                }
+        
+            }
+        );
+
+    }, [address])
+  
     return <div>
         
 
