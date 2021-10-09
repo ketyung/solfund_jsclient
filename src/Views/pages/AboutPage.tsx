a3e8d8784d3cc8e5e01dd9b18daeca6d8a5c26e2
techchee <ketyung@techchee.com>
2021-10-09 16:43:40 +0800

    update

--- src/Views/pages/AboutPage.tsx
+++ src/Views/pages/AboutPage.tsx
@@ -18,7 +18,7 @@ export const AboutPage : React.FC = () => {
 
                 </p>
 
-                <p>Designed, Developed &amp; Expertised By Christopher K Y Chee (ketyung@techchee.com 
+                <p>Designed &amp; Developed By Christopher K Y Chee (ketyung@techchee.com 
                     &nbsp;<a target="_blank" href="https://github.com/ketyung/">https://github.com/ketyung/</a>) for 
                 Solana Ignition Hackathon</p> 
                 <p>Original Idea By Marcus Yong</p>
